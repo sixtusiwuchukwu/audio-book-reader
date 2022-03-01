85cc17d1194760ab18c25a7b5ebc64af9e2f3f5d
sixtus iwuchukwu <sixtusiwuchukwu21@gmail.com>
2022-03-01 10:34:27 +0100

    first commit

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
-const video = document.querySelector("video")
-const textElem = document.querySelector("[data-text]")
+const video = document.querySelector("video");
+const button = document.querySelector("button");
+const textElem = document.querySelector("[data-text]");
 
-async function setup(){
- const stream = await navigator.mediaDevices.getUserMedia({video:"true"})
- video.srcObject = stream
+async function setup() {
+  const stream = await navigator.mediaDevices.getUserMedia({ video: "true" });
+  video.srcObject = stream;
 
- video.addEventListener("playing",async ()=>{
-     const worker = Tesseract.createWorker()
-     await worker.load()
-     await worker.loadLanguage("eng")
-     await worker.initialize("eng")
+  video.addEventListener("playing", async () => {
+    const worker = Tesseract.createWorker();
+    await worker.load();
+    await worker.loadLanguage("eng");
+    await worker.initialize("eng");
 
+    const canvas = document.createElement("canvas");
+    canvas.width = video.width;
+    canvas.height = video.height;
+    button.addEventListener("click", async (e) => {
+      if (e.code !== "Space") return;
 
-     const canvas = document.createElement("canvas")
-     canvas.width = video.width
-     canvas.height = video.height
-document.addEventListener('keypress',async e=>{
-    if(e.code !== "Space")return
-
-    canvas.getContext('2d').drawImage(video,0,0,video.width,video.height)
-    const {data : {text}} = await worker.recognize(canvas)
-    speechSynthesis.speak(new SpeechSynthesisUtterance(text.replace(/\s/g,"")))
-    textElem.textContent = text
-})
-
- })
+      canvas.getContext("2d").drawImage(video, 0, 0, video.width, video.height);
+      const {
+        data: { text },
+      } = await worker.recognize(canvas);
+      speechSynthesis.speak(
+        new SpeechSynthesisUtterance(text.replace(/\s/g, ""))
+      );
+      textElem.textContent = text;
+    });
+  });
 }
 
-setup()
+setup();
